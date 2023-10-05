43c6a4d4a3c8945fd60ffe97c4566427e05c8e0d
kyle-lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-10-05 13:11:46 -0400

    Added Code

--- main.ts
+++ main.ts
@@ -1,8 +1,54 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyle Lyver
+ * Created on: Oct 2023
+ * This program shows all of the color possibilites of a RGB LED
 */
 
-basic.showString('Hello, World!')
+// start code red
+input.onButtonPressed(Button.A, function() {
+pins.digitalWritePin(DigitalPin.P16, 1)
+basic.showString("Red")
+basic.pause(1000)
+pins.digitalWritePin(DigitalPin.P16, 0)
+// Green
+pins.digitalWritePin(DigitalPin.P15, 1)
+basic.showString("Green")
+basic.pause(1000)
+pins.digitalWritePin(DigitalPin.P15, 0)
+// Blue
+pins.digitalWritePin(DigitalPin.P14, 1)
+basic.showString("Blue")
+basic.pause(1000)
+pins.digitalWritePin(DigitalPin.P14, 0)
+// Magenta
+pins.digitalWritePin(DigitalPin.P16, 1)
+pins.digitalWritePin(DigitalPin.P14, 1)
+basic.showString("Magenta")
+basic.pause(1000)
+pins.digitalWritePin(DigitalPin.P16, 0)
+pins.digitalWritePin(DigitalPin.P14, 0)
+// Yellow
+pins.digitalWritePin(DigitalPin.P16, 1)
+pins.digitalWritePin(DigitalPin.P15, 1)
+basic.showString("Yellow")
+basic.pause(1000)
+pins.digitalWritePin(DigitalPin.P16, 0)
+pins.digitalWritePin(DigitalPin.P15, 0)
+// Cyan
+pins.digitalWritePin(DigitalPin.P16, 1)
+pins.digitalWritePin(DigitalPin.P14, 1)
+basic.showString("Cyan")
+basic.pause(1000)
+pins.digitalWritePin(DigitalPin.P16, 0)
+pins.digitalWritePin(DigitalPin.P14, 0)
+// White
+pins.digitalWritePin(DigitalPin.P16, 1)
+pins.digitalWritePin(DigitalPin.P14, 1)
+pins.digitalWritePin(DigitalPin.P15, 1)
+basic.showString("White")
+basic.pause(1000)
+pins.digitalWritePin(DigitalPin.P16, 0)
+pins.digitalWritePin(DigitalPin.P15, 0)
+pins.digitalWritePin(DigitalPin.P14, 0)
+})
