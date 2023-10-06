b72366ddeedeb7ea2504477a1f81265a09feaf64
kyle-lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-10-06 19:06:26 +0000

    Python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,48 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Kyle Lyver
+Created on: Oct 2023
+This module shows all of the color possibilites of a RGB LED on a Micro:Bit
 """
 
 from microbit import *
 
-
-display.scroll("Hello, World!")
+while True:
+    if button_a.is_pressed():
+        pin16.write_digital(1)
+        display.scroll("Red")
+    sleep(1)
+    pin16.write_digital(0)
+    pin15.write_digital(1)
+    display.scroll("Green")
+    sleep(1)
+    pin15.write_digital(0)
+    pin14.write_digital(1)
+    display.scroll("Blue")
+    sleep(1)
+    pin14.write_digital(0)
+    pin16.write_digital(1)
+    pin14.write_digital(1)
+    display.scroll("Magenta")
+    sleep(1)
+    pin16.write_digital(0)
+    pin14.write_digital(0)
+    pin16.write_digital(1)
+    pin15.write_digital(1)
+    display.scroll("Yellow")
+    sleep(1)
+    pin16.write_digital(0)
+    pin15.write_digital(0)
+    pin15.write_digital(1)
+    pin14.write_digital(1)
+    display.scroll("Cyan")
+    sleep(1)
+    pin15.write_digital(0)
+    pin14.write_digital(0)
+    pin16.write_digital(1)
+    pin14.write_digital(1)
+    pin15.write_digital(1)
+    display.scroll("White")
+    sleep(1)
+    pin16.write_digital(0)
+    pin15.write_digital(0)
+    pin14.write_digital(0)
